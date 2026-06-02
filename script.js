9eb83d4d86bcc7c3de312aae7753b2880a6514f8
larreajordan-eng <266338470+larreajordan-eng@users.noreply.github.com>
2026-06-01 21:05:07 -0700

    Make human choice case insensitive utilize constatnts to make human input case insensitive

--- script.js
+++ script.js
@@ -22,4 +22,39 @@ getComputerChoice
 function getHumanChoice() {
     return prompt('Select Rock, Paper, or Scissors');
 }
-console.log(getHumanChoice());
+const human = getHumanChoice();
+const lowerHuman = human.toLowerCase();
+const humanChoice = lowerHuman.charAt(0).toUpperCase() + lowerHuman.slice(1);
+console.log(humanChoice);
+
+
+//Score variables
+let humanScore = 0;
+let computerScore = 0;
+
+/*
+//Stack overflow error
+function playRound(getHumanChoice, getComputerChoice) {
+    if (playRound('Rock', 'Rock')) {
+        console.log('A tie!');
+    } else if (playRound('Rock', 'Paper')) {
+        console.log('You Win!');
+    } else if (playRound('Rock', 'Scissors')) {
+        console.log('You Lose!');
+    } else if (playRound('Paper', 'Rock')) {
+        console.log('You Win!');
+    } else if (playRound('Paper', 'Paper')) {
+        console.log('A Tie!');
+    } else if (playRound('Paper', 'Scissors')) {
+        console.log('You Lose!');
+    } else if (playRound('Scissors', 'Rock')) {
+        console.log('You Lose!');
+    } else if (playRound('Scissors', 'Paper')) {
+        console.log('You Win!');
+    } else if (playRound('Scissors', 'Scissors')) {
+        console.log('A Tie!');
+    }
+}
+playRound();
+
+*/ 
