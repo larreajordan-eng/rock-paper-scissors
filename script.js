0fbffd2ced652ad569b8b70fe1de65dc17aeca2d
larreajordan-eng <266338470+larreajordan-eng@users.noreply.github.com>
2026-06-03 21:41:01 -0700

    Update logic in computer choice and game logic

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 //Will return computer's selection in console
 function getComputerChoice() {
-    return Math.random()
-}
-
-const choice = getComputerChoice();
+    const randomNumber = Math.random()
 
 
-
-if (choice <= 0.32) {
-    console.log('Rock');
-} else if (choice <= 0.65 && choice >= 0.33) {
-    console.log('Paper');
+if (randomNumber <= 0.32) {
+    return 'Rock';
+} else if (randomNumber <= 0.65 && choice >= 0.33) {
+    return 'Paper';
 } else {
-    console.log('Scissors');
+    return 'Scissors';
+}
 }
 
-getComputerChoice
 
 
 //Human choice
@@ -24,37 +20,39 @@ function getHumanChoice() {
 }
 const human = getHumanChoice();
 const lowerHuman = human.toLowerCase();
-const humanChoice = lowerHuman.charAt(0).toUpperCase() + lowerHuman.slice(1);
-console.log(humanChoice);
+const humanChoiceCaseInsensitive = lowerHuman.charAt(0).toUpperCase() + lowerHuman.slice(1);
+console.log(humanChoiceCaseInsensitive);
 
 
 //Score variables
 let humanScore = 0;
 let computerScore = 0;
 
-/*
-//Stack overflow error
-function playRound(getHumanChoice, getComputerChoice) {
-    if (playRound('Rock', 'Rock')) {
+
+
+
+function playRound(humanChoice, computerChoice) {
+    const matchUp = `${humanChoice}-${computerChoice}`
+    switch (matchUp) {
+    
+        case 'Rock-Rock':
+        case 'Paper-Paper':
+        case 'Scissors-Scissors':
         console.log('A tie!');
-    } else if (playRound('Rock', 'Paper')) {
-        console.log('You Win!');
-    } else if (playRound('Rock', 'Scissors')) {
-        console.log('You Lose!');
-    } else if (playRound('Paper', 'Rock')) {
-        console.log('You Win!');
-    } else if (playRound('Paper', 'Paper')) {
-        console.log('A Tie!');
-    } else if (playRound('Paper', 'Scissors')) {
-        console.log('You Lose!');
-    } else if (playRound('Scissors', 'Rock')) {
-        console.log('You Lose!');
-    } else if (playRound('Scissors', 'Paper')) {
-        console.log('You Win!');
-    } else if (playRound('Scissors', 'Scissors')) {
-        console.log('A Tie!');
+        break;
+
+        case 'Rock-Scissors':
+        case 'Paper-Rock':
+        case 'Scissors-Paper':
+        console.log('You win!');
+        humanScore++
+        break;
+
+        case 'Rock-Paper':
+        case 'Paper-Scissors':
+        case 'Scissors-Rock':
+        console.log('You lose.');
+        computerScore++;
+        break;
     }
 }
-playRound();
-
-*/ 
